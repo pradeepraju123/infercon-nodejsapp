336ce4b507e313dc05831e951ffdf39e1b5fdedb
nanthini <nanthini@trstscore.com>
2025-05-30 09:57:48 +0530

    newupdate

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -14,7 +14,7 @@ module.exports = app => {
      router.post("/bulkupload", authenticateToken,upload.single("file"),users.excelupload);
     //router.post("/bulkupload",upload.single("file"),users.excelupload);
 
-    router.post("/bulkwhatsmes",users.bulkExcelMes);
+    router.post("/bulkwhatsmes",authenticateToken,users.bulkExcelMes);
     router.get("/getall",authenticateToken,users.allcontacts);
     //retrieve 
     router.post("/filtercontact",users.filtercontact);
