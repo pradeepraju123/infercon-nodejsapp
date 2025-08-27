d59d3a25a093b7645123bfbf22d79beca29e5923
pradeepraju123 <rpradeep1797@gmail.com>
2025-08-28 00:48:39 +0530

    possible fix

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb+srv://iotdb:qp!bJ_FkkfzhKr5@mydbcloud.tztsqmz.mongodb.net/demo",
+  url: "mongodb+srv://iotdb:qp!bJ_FkkfzhKr5@mydbcloud.tztsqmz.mongodb.net/test",
   secret_key : "kX2lNYlMYN6pKcecZiLGJeEsm7HrsOAj"
 };
